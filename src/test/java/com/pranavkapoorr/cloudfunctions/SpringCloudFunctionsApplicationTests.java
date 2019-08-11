4c8bd89dad071ad20311d242e7574c7548d9577c
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-08-10 23:04:33 -0400

    helloworld test

--- src/test/java/com/pranavkapoorr/cloudfunctions/SpringCloudFunctionsApplicationTests.java
+++ src/test/java/com/pranavkapoorr/cloudfunctions/SpringCloudFunctionsApplicationTests.java
@@ -1,16 +1,22 @@
 package com.pranavkapoorr.cloudfunctions;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringCloudFunctionsApplicationTests {
-
+	@Autowired
+	HelloWorldFunctions func;
+	
 	@Test
-	public void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals("HelloWorldCloud function test!", "HelloWorld", func.helloWorld().get());
 	}
 
 }
